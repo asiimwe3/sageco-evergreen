53965775f763e615765362a965c1cb055548eb5c
asiimwe3 <derickasiimwe849@gmail.com>
2026-05-14 19:51:52 +0300

    feat(seo,a11y): add global meta charset, viewport, theme-color to _app

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,17 @@
-import '../styles/globals.css'
+import "../styles/globals.css"
+import Head from "next/head"
+
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="theme-color" content="#1a5c38" />
+        <meta name="author" content="SAGECO EVERGREEN" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
