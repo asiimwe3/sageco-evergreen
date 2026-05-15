6dc45eb71f5de009785178d8eaf8c00bd67256fb
asiimwe3 <derickasiimwe849@gmail.com>
2026-05-15 06:21:12 +0300

    Add apple-touch-icon and favicon meta tags

--- pages/_app.js
+++ pages/_app.js
@@ -9,7 +9,9 @@ export default function App({ Component, pageProps }) {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta name="theme-color" content="#1a5c38" />
         <meta name="author" content="SAGECO EVERGREEN" />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+        <link rel="shortcut icon" href="/favicon.ico" />
       </Head>
       <Component {...pageProps} />
     </>
