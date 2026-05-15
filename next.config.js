a53b81eb277987a7f2483c6ce677894bca2d13bb
asiimwe3 <derickasiimwe849@gmail.com>
2026-05-15 06:26:02 +0300

    Enable unoptimized images for Supabase storage URLs

--- next.config.js
+++ next.config.js
@@ -8,6 +8,7 @@ const nextConfig = {
     ],
     formats: ["image/avif", "image/webp"],
     deviceSizes: [640, 750, 828, 1080, 1200, 1920],
+    unoptimized: true,
   },
   async headers() {
     return [
