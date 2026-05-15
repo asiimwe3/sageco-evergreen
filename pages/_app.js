d22db1f9e4f6a6f2a04ec621045e1562ac9ca1cd
asiimwe3 <derickasiimwe849@gmail.com>
2026-05-15 10:11:29 +0300

    feat: integrate ChatBot globally in _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 import "../styles/globals.css"
 import Head from "next/head"
 import { AuthProvider } from "../context/AuthContext"
+import ChatBot from "../components/ChatBot"
 
 export default function App({ Component, pageProps }) {
   return (
@@ -15,6 +16,7 @@ export default function App({ Component, pageProps }) {
         <link rel="shortcut icon" href="/favicon.ico" />
       </Head>
       <Component {...pageProps} />
+      <ChatBot />
     </AuthProvider>
   )
 }
